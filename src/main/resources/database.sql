1e9928c815a6b30227c560fcde2ec40bf56d3dc7
volginys <volginys12@mail.ru>
2021-05-05 00:19:10 +0300

    исправление №1

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -4,7 +4,8 @@ CREATE TABLE users (
                        email VARCHAR(255) NOT NULL,
                        first_name VARCHAR(255) NOT NULL,
                        last_name VARCHAR(255) NOT NULL,
-                       password VARCHAR(255) NOT NULL
+                       password VARCHAR(255) NOT NULL,
+                       UNIQUE (email)
 )
     ENGINE = InnoDB;
 
