f75507b34d4e86fa50551fe8a16dbf84fe0600f0
volginys <volginys12@mail.ru>
2021-05-18 09:42:15 +0300

    Исправление №4

--- src/main/java/web/config/formatter/RoleStringConverter.java
+++ src/main/java/web/config/formatter/RoleStringConverter.java
@@ -4,7 +4,6 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 import web.model.Role;
 
-
 @Component
 public class RoleStringConverter implements Converter<Role, String> {
 
